0754ab66bdae2312d1a436e81c127eebe1ca296e
yannickmacken <yannickmacken@gmail.com>
2022-10-17 09:01:12 +0200

    usenavigate to get back to welcome page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
+
+    let navigate = useNavigate();
+    function handleClick() {
+      navigate("/");
+    }
+
     return (
         <div>
             <p>Page not found!</p>
+            <button onClick={handleClick}>go to welcome page</button>
         </div>
     )
 }
