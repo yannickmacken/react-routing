7ff6956d954ba0c282568e8f583df844c2bb4326
yannickmacken <yannickmacken@gmail.com>
2022-10-18 09:04:51 +0200

    query params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,43 @@
 import { Link } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 function Products() {
+    let navigate = useNavigate()
+    const location = useLocation()
+    const [ products, setProducts] = useState([ '/products/1', '/products/2' ])
+
+    // Get current URL query params
+    const queryParams = new URLSearchParams(location.search)
+    const isSortingAscending = queryParams.get('sort') === 'asc'
+    
+    // Sort products on basis of URL query params
+    function onClickHandler() {
+        if (isSortingAscending) {
+            navigate('/products?sort=desc')
+            setProducts(a => a.sort())
+        }
+        else {
+            navigate('/products?sort=asc')
+            setProducts(a => a.sort().reverse())
+        }
+    } 
+    console.log(products)
+
+    // Dynamically build sorted link list
+    let links = []
+    for (let [index, productName] of products.entries()) {
+        links.push(<li key={index}><Link to={productName}>{productName}</Link></li>)
+    }
+
     return (
         <div>
             <h1>Products component text</h1>
+            <br/>
+            <button onClick={onClickHandler}>Sort</button>
+            <br/>
             <ul>
-                <Link to={'/products/1'}>Product 1</Link>
-                <Link to={'/products/2'}>Product 2</Link>
+            {links}
             </ul>
         </div>
     )
